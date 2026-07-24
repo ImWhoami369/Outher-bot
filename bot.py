f0e2566045d3acabf3dcee64aa65190098b05762
ImWhoami369 <fadinhadomau@gmail.com>
2026-07-24 16:56:58 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from telebot import types
 # 1. CONFIGURAÇÕES E CHAVES
 # ======================================================
 TOKEN = '8738063689:AAFz8wY1zaE3BixUupl0RBQ9exiy5l8260U'
-CHAT_ID = '-1003981624573'
+CHAT_ID = '8738063689'
 RENDER_URL = "https://bottelegram-1-jtr3.onrender.com"  # Substitua pela sua URL do Render quando subir
 
 bot = telebot.TeleBot(TOKEN)
